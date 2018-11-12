a36f0443fd2f5196465b79195f87134bb7e03f87
Nicolas Hennick <nicolashennick@gmail.com>
2018-11-12 15:26:39 +0000

    completed methods bicycles controller

--- app/controllers/bicycles_controller.rb
+++ app/controllers/bicycles_controller.rb
@@ -1,10 +1,11 @@
 class BicyclesController < ApplicationController
+  before_action :find_bicycle, only: [:show, :edit, :update, :destroy]
+
   def index
     @bicycles = Bicycle.all
   end
 
   def show
-    @bicycle = Bicycle.find(params[:id])
   end
 
   def new
@@ -12,24 +13,34 @@ class BicyclesController < ApplicationController
   end
 
   def create
+    @user = User.find(params[:user_id])
     @bicycle = Bicycle.new(bicycle_params)
+    if @user.save
+      redirect_to bicycle_path(@bicycle)
+    else
+      render :new
+    end
   end
 
   def edit
-
   end
 
   def update
-
+    @bicycle.update(bicycle_params)
+    redirect_to bicycle_path(@bicycle)
   end
 
   def destroy
-
+    @bicycle.destroy
   end
 
   private
 
   def bicycle_params
     params.require(:bicycle).permit(:price, :address, :user_id)
+  end
+
+  def find_bicycle
+    @bicycle = Bicycle.find(params[:id])
   end
 end
