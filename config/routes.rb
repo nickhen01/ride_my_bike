6fead1c9049c65cce3176878f58434aef74d547e
Ariel LeRoy <arielmatsuileroy@gmail.com>
2018-11-12 14:40:58 +0000

    added necessary routes to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'bicycles#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show, :edit, :update, :destroy] do
+    resources :bicycles, only: [:new, :create, :edit, :update, :destroy]
+  end
+  resources :bicycles, only: [:index, :show]
 end
