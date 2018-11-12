728f232e83b060e19e53888cb165bd607d283a0a
Nicolas Hennick <nicolashennick@gmail.com>
2018-11-12 17:22:36 +0000

    added banner

---
+++ app/javascript/components/navbar.js
@@ -0,0 +1,19 @@
+function initUpdateNavbarOnScroll() {
+  const banner = document.querySelector('.banner');
+  const navbar = document.querySelector('.navbar-wagon');
+  if (banner) {
+    if (navbar) {
+      window.addEventListener('scroll', () => {
+        if (window.scrollY >= window.innerHeight) {
+          navbar.classList.add('navbar-wagon-white');
+        } else {
+          navbar.classList.remove('navbar-wagon-white');
+        }
+      });
+    }
+  } else {
+    navbar.classList.add('navbar-wagon-white')
+  }
+}
+
+export { initUpdateNavbarOnScroll };
