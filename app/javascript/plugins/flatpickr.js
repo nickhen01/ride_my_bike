a8f57fba66f99044df6d4cd4d73f38f5afa742a8
michael gregory <michaelgregory8976@gmail.com>
2018-11-13 18:00:29 +0000

    nearlly finsihed with price calculation

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,6 +1,21 @@
 import flatpickr from "flatpickr"
-import "flatpickr/dist/flatpickr.min.css"
+import "flatpickr/dist/themes/airbnb.css"
 
-flatpickr(".datepicker", {})
+flatpickr(".datepicker", {
+  altInput: true,
+  enableTime: true,
+  dateFormat: "Y-m-d H:i"
+})
+// Bens Genius Code
+// finishedStartTime = parseInt(document.getElementById('search_pick_up').value.split(' ')[0].split('-').join(''))
+// finishedEndTime = parseInt(document.getElementById('search_drop_off').value.split(' ')[0].split('-').join(''))
 
-app/assets/stylesheets/components/date.scss
+flatpickr({
+    "plugins": [new rangePlugin({ input: "#secondRangeInput"})]
+});
+
+var a = document.getElementById('search_pick_up').value
+var b = document.getElementById('search_drop_off').value
+var milliseconds = ((new Date(a)) - (new Date(b)))
+var minutes = milliseconds/(60000)
+var hours = minutes/(60)
