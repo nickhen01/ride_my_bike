1451d1bacd31e9c7a8cfd61f7789f01993b5c9b5
michael gregory <michaelgregory8976@gmail.com>
2018-11-16 14:21:10 +0000

    navbar patched and now transitioning again

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,10 +1,12 @@
 function initUpdateNavbarOnScroll() {
   const banner = document.querySelector('.banner');
   const navbar = document.querySelector('.navbar-wagon');
+  console.log(banner)
+  console.log(navbar)
   if (banner) {
     if (navbar) {
       window.addEventListener('scroll', () => {
-        if (window.scrollY > 270) {
+        if (window.scrollY > 10) {
           navbar.classList.add("navbar-wagon-white");
         } else {
           navbar.classList.remove('navbar-wagon-white');
@@ -17,4 +19,6 @@ function initUpdateNavbarOnScroll() {
 }
 
 export { initUpdateNavbarOnScroll };
+
+
 
